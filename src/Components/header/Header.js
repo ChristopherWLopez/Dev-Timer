08f00eb172817ebd50a24721a356db09c0417ccb
christopher lopez <christopherwlopez89@gmail.com>
2023-08-12 13:27:22 -0700

    scaffolding set up and initializing functional components

---
+++ src/Components/header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Please set you desired interval!</h1>
+    </div>
+  )
+}
---
+++ src/Components/main/Main.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+//9000000 is about 15 minutes and that is the desired lenght of time you want to work on a task without asking for help
+
+export default function Main () {
+
+    // useEffect(()=>{
+
+        
+    //     const timeout = 20000
+    //     setTimeout(()=> {
+    //         console.log("Hello There")
+    //     }, timeout);
+
+    //     return clearTimeout(timeout);
+
+    // },[])
+        return (
+    
+        <h1>"Hello"</h1>
+    
+  );
+}
