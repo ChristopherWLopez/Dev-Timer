6362070ed01d5d483eabad60197b5c98233b81b6
christopher lopez <christopherwlopez89@gmail.com>
2023-09-12 18:36:48 -0700

    chat id up and running and working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Components/Chat-Feature/Client/Home';
 import Header from "./Components/header/header/Header"
 import Main from "./Components/main/Main.js";
 import Footer from "./Components/footer/Footer";
-import socketIO from ' socket.io-client';
+import socketIO from 'socket.io-client';
 import './App.css';
+import { Router } from "express";
 
 const socket = socketIO.connect('http://localhost:4000');
 
 
 function App() {
-
   
   return (
-    <div className="App">
-      <Header/>
-      <Main/>
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home socket={socket}/>}></Route>
+          <Header/>
+          <Main/>
+          <Footer/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
