1e50f8797da821287b3fd907ea8f5ff4ae3560fd
christopher lopez <christopherwlopez89@gmail.com>
2023-09-12 18:18:12 -0700

    working with the server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Header from "./Components/header/header/Header"
 import Main from "./Components/main/Main.js";
 import Footer from "./Components/footer/Footer";
+import socketIO from ' socket.io-client';
 import './App.css';
 
+const socket = socketIO.connect('http://localhost:4000');
+
+
 function App() {
+
+  
   return (
     <div className="App">
       <Header/>
--- src/Components/main/Main.js
+++ src/Components/main/Main.js
@@ -3,7 +3,7 @@ import Timer from "../timers/PomodoroTimer/Timer"
 import BreakTimer from '../timers/BreakTimer/BreakTimer';
 // import OpenAI from "../ChatGPT/ChatGPTAPI";
 // import MyChatComponent from '../ChatGPT/ChatAPI';
-import ChatBot from "../ChatGPT/ChatG";
+// import ChatBot from "../ChatGPT/ChatG";
 // 
 //9000000 is about 15 minutes and that is the desired lenght of time you want to work on a task without asking for help
 
